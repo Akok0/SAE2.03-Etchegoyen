6f158dd7f66ab1f6107b1f296415606d2ba96e44
Etchegoyen Malo <etchegoyen3@mmi-dev.unilim.fr>
2026-04-22 18:00:57 +0200

    ITÉRATION 1 noCss

---
+++ app/component/Movies/script.js
@@ -0,0 +1,20 @@
+let templateFile = await fetch("./component/Movies/template.html");
+let templateFileMovie = await fetch("./component/Movies/templateMovie.html");
+let template = await templateFile.text();
+let templateMovie = await templateFileMovie.text();
+
+let Movies = {};
+
+Movies.format = function (data, tab) {
+  let html = template;
+  let htmlMovie = "";
+  for (let movie of data){
+    let card = templateMovie;
+    card = card.replaceAll("{{name}}", movie.name).replaceAll("{{image}}", "../server/images/" + movie.image)
+    htmlMovie += card;
+  }
+  html = html.replace("{{movie}}", htmlMovie)
+  return html;
+};
+
+export { Movies };
